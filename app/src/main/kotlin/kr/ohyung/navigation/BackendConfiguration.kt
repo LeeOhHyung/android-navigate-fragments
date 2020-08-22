fc8ca8d18dd6b13536afd0d1cd612f4ab6d36a8b
LeeOhHyung <lee2928030@naver.com>
2020-08-22 21:32:01 +0900

    feat: remove deprecated classes that backendconfiguration, retrofitmanager

--- app/src/main/kotlin/kr/ohyung/navigation/BackendConfiguration.kt
+++
@@ -1,7 +0,0 @@
-/*
- * Created by Lee Oh Hyoung on 2020/08/06 .. 
- */
-package kr.ohyung.navigation
-
-@Deprecated("deprecated because of hilt dependency injection")
-internal data class BackendConfiguration(val baseUrl: String)
